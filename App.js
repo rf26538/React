2a20ffc3371836d1812b9972cc77776e6a5aca6d
manisht4 <manisht4@chetu.com>
2024-08-31 20:06:13 +0530

    Make dynamic components and use of props

--- App.js
+++ App.js
@@ -1,33 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
+
+const styleCard = {
+    backgroundColor : "#f0f0f0",
+}
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <img 
+                className="logo"
+                src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
 
 /**
- * Js is mix of javascript and html
- * Components inside a component is called component composition
- * {} we can write any javasript code inside the curly barces
+ * We can destructure props like this
+ * const Card = ({resName, cusine, rating, time}) => { return (<h4>{cusine}</h4>) }
  */
 
+const Card = (props) => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <img
+            className="res-logo"
+             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/9/1d6a4dbe-8836-4bad-9733-ccf51912067a_90170.jpg" alt="res-logo"></img>
+            <h3>{props.resName}</h3>
+            <h4>{props.cusine}</h4>
+            <h4>{props.rating}stars</h4>
+            <h4>{props.time} minutes</h4>
+        </div>
+    );
+}
 
-const Title = () => (
-    <div className="newComp" id="he">
-        <h3>{10*1000}</h3>
-        <h2>I am new comp</h2>
-    </div>
-)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <Card resName="Megha Foods" rating="4.2" time="30" cusine="Biryani, Asian "/>
+                <Card resName="KFC" rating="4.0" time="42" cusine="Asian"/>
+            </div>
+        </div>
+    );
+}
 
-const element = (
-    <h3>Add element</h3>
-)
-const data  = "new data";
-const data1  = 58112;
-const HeadingComponent = () => (
-    <div className="test">
-        {element}
-        <Title />
-        <h1 id="tes">React Javascript</h1>
-        <p>{data} {data1}</p>
-    </div>  
-)
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <footer className="foot">
+                <div className="company-info">
+                    <span className="companey-text">© 2022 Company, Inc</span>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="conatiner">
+           <Header/>
+           <Body/>
+           <Footer/>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
