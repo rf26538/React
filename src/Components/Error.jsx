6c933229f98edb084dbdf97c4b7c512aea026612
Rehan Fazal <rf26538@gmail.com>
2024-09-08 07:23:44 +0530

    Show errors using react router dom

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,8 +1,17 @@
+/**
+ * Get more info of error using react router dom
+ */
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+    
     return (
         <>
             <h1>Error</h1>
             <p>Oops Something went wrong</p>
+            <h3>{ err.status } : { err.statusText }</h3>
         </>
     )
 }
