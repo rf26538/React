ffdc2fb10274ea9c576b384b7ff8013920b6f5ff
Rehan Fazal <rf26538@gmail.com>
2024-09-08 07:50:17 +0530

    Navigate using Link and RouteProvider from router

--- src/App.js
+++ src/App.js
@@ -3,38 +3,62 @@ import ReactDOM from "react-dom/client";
 import '../index.css';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Body from "./components/Body";
+import Body from "./Components/Body";
 import About from "./Components/About";
 import ContactUs from "./Components/ContactUs";
 import Error from "./Components/Error";
 
 /**React Router */
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
     return (
         <div className="conatiner">
             <Header />
-            <Body />
-            <Footer />
+            <Outlet />
+            {/* <Footer /> */}
         </div>
     );
 }
+
+// const appRouter = createBrowserRouter([
+//     {
+//         path : "/",
+//         element: <AppLayout />,
+//         errorElement : <Error />
+//     },
+//     {
+//         path : "/about",
+//         element: <About />
+//     },
+//     {
+//         path : "/contact",
+//         element: <ContactUs />
+//     },
+// ])
+
+/**Children Routing to change only body not header also */
 
 const appRouter = createBrowserRouter([
     {
         path : "/",
         element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path : "/about",
+                element: <About />
+            },
+            {
+                path : "/contact",
+                element: <ContactUs />
+            },
+        ],
         errorElement : <Error />
-    },
-    {
-        path : "/about",
-        element: <About />
-    },
-    {
-        path : "/contact",
-        element: <ContactUs />
-    },
+    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,6 +1,12 @@
 import {LOGO_URL} from "../utils/constants";
 import { useState } from "react";
 
+/** 
+ * Do not use achor tag use this for navigation
+ * This will do not let the page refresh
+ *  */
+import { Link } from "react-router-dom";
+
 const Header = () => {
     const [btnName, setBtnName] = useState("login");
     return (
@@ -12,9 +18,15 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li>
+                    <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
                     <li>Cart</li>
                     <button className="login" onClick={() => {
                         btnName === "login" ? setBtnName("logout") : setBtnName("login");
