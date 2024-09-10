c16ba39eff742e6fb9bf7612232954807e45c23a
Rehan Fazal <rf26538@gmail.com>
2024-09-11 05:21:48 +0530

    Dynamic routing

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -1,26 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "../Components/Shimmer";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
 
-    useEffect(() => {
+    const [resInfo, setResInfo] = useState(null);
 
+    useEffect(() => {
+        fetchMenu();
     }, []);
 
     const fetchMenu = async () => {
-        const data = fetch("")
+        const data = await fetch(MENU_API + "restaurantId=425&submitAction=ENTER")
+        const json = await data.json();
+
+        const menuInfo = json?.data?.cards[2]?.card?.card?.info;
+        console.log(menuInfo);
+        setResInfo(menuInfo);  
     }
 
-   return <div className="menu">
-            <h1>Name of restaurant</h1>
-            <h2>Menu</h2>
-
+    // const {name, cuisines, cloudinaryImageId, costForTwoMessage, totalRatingsString} = resInfo;
+    return resInfo === null ? (
+        <Shimmer />
+    ) : (
+        <div className="menu">
+            <h1>{resInfo.name}</h1>
+            <h2>{resInfo.cuisines.join(", ")}</h2>
+            <h2>{resInfo.costForTwoMessage}</h2>
+            <h2>{resInfo.totalRatingsString}</h2>
             <ul>
-                <li>Biryani</li>
-                <li>Burger</li>
+                {resInfo}
+                <li></li>
+                <li></li>
                 <li>Pizaa</li>
                 <li>coke</li>
             </ul>
         </div>
+    )
 }
 
 export default RestaurantMenu;
