cd8642a3adc458cf4b45e52f38a1f739080e51c8
Rehan Fazal <rf26538@gmail.com>
2024-10-26 08:47:35 +0530

    Shimmer Example

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -6,6 +6,21 @@ const Shimmer = () => {
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
         </div>
     )
 }
